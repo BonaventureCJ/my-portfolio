d0c6e84dca2ab1c632378b8907a06dc542788e8b
Bonaventure CJ <ugwubonaventure@gmail.com>
2025-09-03 22:13:24 +0100

    feat(pages): Add initial placeholder pages for new feature [WIP]

---
+++ src/app/home/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <div>Home</div>
+    <p>The Home that is rendered here is at /Home and not at /</p>
+    </>
+  )
+}
+
+export default page
